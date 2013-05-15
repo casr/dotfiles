8c05283f29ce6e28fcc7935a2df0e2a97f1813bd
Chris Rawnsley <casr@darkcog.com>
2013-05-15 19:24:13 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+LN=${LN:-ln -sh}            # Default link command
+
+BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
+BASEDIR=${BASEDIR#$HOME/}     # Saner links
+
+${LN} $BASEDIR/vim   $HOME/.vim
+${LN} $BASEDIR/vimrc $HOME/.vimrc
