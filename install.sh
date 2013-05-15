99bf6a29ac4e971058cbbcad505c299304e6eb67
Chris Rawnsley <casr@darkcog.com>
2013-05-15 22:56:58 +0100

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,6 @@
+LN=${LN:-ln -sh}              # Default link command
+
+BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
+BASEDIR=${BASEDIR#$HOME/}     # Saner links
+
+${LN} $BASEDIR/tmux.conf   $HOME/.tmux.conf
