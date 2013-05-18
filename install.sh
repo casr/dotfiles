c004dd2cab71276950baf05b59ea0cf33cd72b18
Chris Rawnsley <casr@darkcog.com>
2013-05-18 17:16:56 +0100

    GNU ln doens't recognise -h

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-LN=${LN:-ln -sh}              # Default link command
+LN=${LN:-ln -sn}              # Default link command
 
 BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
 BASEDIR=${BASEDIR#$HOME/}     # Saner links
