3addf01aa7932576ffa17d98ee227275b68f9e22
Chris Rawnsley <casr@darkcog.com>
2013-05-21 16:07:58 +0100

    Always ignore .DS_Store files.

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
 BASEDIR=${BASEDIR#$HOME/}     # Saner links
 
 ${LN} $BASEDIR/gitconfig $HOME/.gitconfig
+${LN} $BASEDIR/gitignore_global $HOME/.gitignore_global
