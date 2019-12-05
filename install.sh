e813f27c095a6c09c8068642b874f7ac55361f5f
Chris Rawnsley <chris@puny.agency>
2019-12-05 23:41:45 +0000

    Prepare files for merging into one repository

--- install.sh
+++
@@ -1,6 +0,0 @@
-LN=${LN:-ln -sn}              # Default link command
-
-BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
-BASEDIR=${BASEDIR#$HOME/}     # Saner links
-
-${LN} $BASEDIR/tmux.conf   $HOME/.tmux.conf
