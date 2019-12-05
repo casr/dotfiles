0df106ad87c4c65bca9c0a46c39ac132c3afb034
Chris Rawnsley <chris@puny.agency>
2019-12-05 23:41:45 +0000

    Prepare files for merging into one repository

--- install.sh
+++
@@ -1,7 +0,0 @@
-LN=${LN:-ln -sn}              # Default link command
-
-BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
-BASEDIR=${BASEDIR#$HOME/}     # Saner links
-
-${LN} $BASEDIR/gitconfig $HOME/.gitconfig
-${LN} $BASEDIR/gitignore_global $HOME/.gitignore_global
