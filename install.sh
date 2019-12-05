73f35fedd741cb3a32ce269faac918131234831b
Chris Rawnsley <chris@puny.agency>
2019-12-05 23:41:45 +0000

    Prepare files for merging into one repository

--- install.sh
+++
@@ -1,7 +0,0 @@
-LN=${LN:-ln -sn}              # Default link command
-
-BASEDIR=$(pwd)/$(dirname $0)  # Link to the right place
-BASEDIR=${BASEDIR#$HOME/}     # Saner links
-
-${LN} $BASEDIR/vim   $HOME/.vim
-${LN} $BASEDIR/vimrc $HOME/.vimrc
