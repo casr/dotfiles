d8f27956fd03018200e4cd04ae820eff60880c1d
Chris Rawnsley <chris@puny.agency>
2023-11-07 07:54:20 +0000

    neovim: update core settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,26 @@
 vim.opt.breakindent = true
-vim.opt.breakindentopt = { "sbr", "shift:2" }
+vim.opt.breakindentopt = { "list:-1", "sbr" }
 vim.opt.colorcolumn = "+1"
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.cursorline = true
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.cpoptions:append({ n = true })
+vim.opt.diffopt = {
+  "algorithm:histogram",
+  "filler",
+  "indent-heuristic",
+  "internal",
+  "linematch:60",
+}
 vim.opt.fillchars = {
   vert = "│",
   foldopen = "┌",
   foldclose = "╓",
   foldsep = "│",
 }
-vim.opt.foldcolumn = "4"
+vim.opt.foldcolumn = "auto:4"
+vim.opt.formatoptions:append({ n = true, o = true, r = true })
 vim.opt.ignorecase = true
 vim.opt.linebreak = true
+vim.opt.list = true
 vim.opt.listchars = {
   tab = "› ",
   trail = "•",
@@ -21,18 +30,33 @@ vim.opt.listchars = {
 }
 vim.opt.modeline = false
 vim.opt.mouse = { c = true, n = true, v = true }
+vim.opt.number = true
+vim.opt.pumheight = 10
 vim.opt.shortmess:append({ I = true })
-vim.opt.showbreak = "…"
+vim.opt.showbreak = "+++ "
 vim.opt.showtabline = 2
-vim.opt.signcolumn = "number"
+vim.opt.signcolumn = "auto:2-4"
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.updatetime = 400
+vim.opt.wildmode = { "longest:full", "full" }
 
 if vim.fn.executable("ag") then
   vim.opt.grepformat = "%f:%l:%c:%m"
   vim.opt.grepprg = "ag --vimgrep $*"
 end
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.g.loaded_2html_plugin = 0
+vim.g.loaded_gzip = 0
+vim.g.loaded_man = 0
+vim.g.loaded_tarPlugin = 0
+vim.g.loaded_tutor_mode_plugin = 0
+vim.g.loaded_zipPlugin = 0
