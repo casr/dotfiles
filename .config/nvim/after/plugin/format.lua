06bf97e285d2217fe20d8b5bf0a2679047d98370
Chris Rawnsley <chris@puny.agency>
2023-11-12 17:42:43 +0000

    neovim: add keymap to toggle auto format

--- .config/nvim/after/plugin/format.lua
+++ .config/nvim/after/plugin/format.lua
@@ -5,6 +5,12 @@ if not r_conform then
 end
 
 conform.setup({
+  format_on_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
   formatters_by_ft = {
     javascript = { "prettier" },
     javascriptreact = { "prettier" },
@@ -19,4 +25,22 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(args)
     conform.format({ bufnr = args.buf })
   end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
 })
