9c3cc998053e5e49eae37224407da3d2bbd22367
Chris Rawnsley <chris@puny.agency>
2023-11-24 08:24:42 +0000

    neovim: move LSP related keymaps with other keymaps

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,3 @@
-local augroup = vim.api.nvim_create_augroup("after_plugin_lsp", {})
-
 local float_config = { width = 60, border = "rounded" }
 
 vim.lsp.handlers["textDocument/hover"] =
@@ -7,20 +5,3 @@ vim.lsp.handlers["textDocument/hover"] =
 
 vim.lsp.handlers["textDocument/signatureHelp"] =
   vim.lsp.with(vim.lsp.handlers.signature_help, float_config)
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = augroup,
-  callback = function(args)
-    local mapopts = { silent = true, buffer = args.buf }
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, mapopts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, mapopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, mapopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, mapopts)
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, mapopts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, mapopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, mapopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, mapopts)
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, mapopts)
-  end,
-})
--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,5 +1,6 @@
 local r_cmp, cmp = pcall(require, "cmp")
 
+local augroup = vim.api.nvim_create_augroup("plugin_keymaps", {})
 local opts = { silent = true }
 
 vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float, opts)
@@ -18,3 +19,20 @@ if r_cmp then
   -- so force the use of nvim-cmp
   vim.keymap.set("i", "<C-x><C-o>", cmp.complete)
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
+  callback = function(args)
+    local mapopts = { silent = true, buffer = args.buf }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, mapopts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, mapopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, mapopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, mapopts)
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, mapopts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, mapopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, mapopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, mapopts)
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, mapopts)
+  end,
+})
