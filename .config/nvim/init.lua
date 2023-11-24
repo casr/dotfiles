0f8fafa968af54a4dc1ea89e054a65f12316f897
Chris Rawnsley <chris@puny.agency>
2023-11-24 08:30:27 +0000

    neovim,vim: improve grepprg args

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,7 +46,7 @@ vim.opt.wildmode = { "longest:full", "full" }
 
 if vim.fn.executable("ag") then
   vim.opt.grepformat = "%f:%l:%c:%m"
-  vim.opt.grepprg = "ag --vimgrep $*"
+  vim.opt.grepprg = "ag --hidden --silent --vimgrep $*"
 end
 
 vim.g.loaded_node_provider = 0
