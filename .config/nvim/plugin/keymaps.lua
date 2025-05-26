47b8581db0443484a8ac8947ff88341587e3d65d
Chris Rawnsley <chris@puny.agency>
2025-05-26 11:31:08 +0100

    neovim: maps to toggle diagnostics and inlay hints

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -4,6 +4,14 @@ vim.keymap.set("n", "<leader>ff", "<Plug>(PickerEdit)", { unique = true })
 vim.keymap.set("n", "<leader>fb", "<Plug>(PickerBuffer)", { unique = true })
 vim.keymap.set("n", "<leader>sh", "<Plug>(PickerHelp)", { unique = true })
 
+vim.keymap.set("n", "yoe", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end)
+
+vim.keymap.set("n", "yog", function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end)
+
 if r_cmp then
   -- regular omnicompletion will fail with the extra capabilities from nvim-cmp
   -- so force the use of nvim-cmp
