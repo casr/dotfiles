c7d4c37dffb6f3f73fda33f1a356607be439fdca
Chris Rawnsley <chris@puny.agency>
2026-02-26 20:27:07 +0000

    nvim-lint: fix annoying linter not found issues

--- .config/nvim/after/plugin/lint.lua
+++ .config/nvim/after/plugin/lint.lua
@@ -21,32 +21,62 @@ lint.linters_by_ft = {
   yaml = { "actionlint" },
 }
 
+local timer = assert(vim.uv.new_timer())
+local DEBOUNCE_MS = 400
+
 vim.api.nvim_create_autocmd(
-  { "BufReadPost", "BufWritePost", "InsertLeave", "TextChanged" },
+  { "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
   {
     group = augroup,
     callback = function(args)
-      local opts = { ignore_errors = args.event ~= "BufWritePost" }
-
-      local clients = vim.lsp.get_clients({ bufnr = 0 })
-      local key, client = next(clients)
-      while key do
-        if client.workspace_folders then
-          for _, dir in pairs(client.workspace_folders) do
-            if vim.fs.relpath(dir.name, vim.api.nvim_buf_get_name(0)) then
-              opts.cwd = dir.name
-            end
+      timer:stop()
+      timer:start(
+        DEBOUNCE_MS,
+        0,
+        vim.schedule_wrap(function()
+          if not vim.api.nvim_buf_is_valid(args.buf) then
+            return
           end
-        elseif client.root_dir then
-          opts.cwd = client.root_dir
-        end
-        if opts.cwd then
-          break
-        end
-        key, client = next(clients, key)
-      end
 
-      lint.try_lint(nil, opts)
+          local names = lint._resolve_linter_by_ft(vim.bo[args.buf].filetype)
+          names = vim.tbl_filter(function(name)
+            local linter = lint.linters[name]
+            assert(linter, "nvim-lint: linter not found " .. name)
+            local cmd = type(linter.cmd) == "function" and linter.cmd()
+              or linter.cmd
+            if vim.fn.exepath(cmd) == "" then
+              vim.notify_once("nvim-lint: cannot find executable " .. cmd)
+              return false
+            end
+            return true
+          end, names)
+
+          local opts = { ignore_errors = args.event ~= "BufWritePost" }
+
+          local clients = vim.lsp.get_clients({ bufnr = 0 })
+          local key, client = next(clients)
+          while key do
+            assert(client)
+            if client.workspace_folders then
+              for _, dir in pairs(client.workspace_folders) do
+                if vim.fs.relpath(dir.name, vim.api.nvim_buf_get_name(0)) then
+                  opts.cwd = dir.name
+                end
+              end
+            elseif client.root_dir then
+              opts.cwd = client.root_dir
+            end
+            if opts.cwd then
+              break
+            end
+            key, client = next(clients, key)
+          end
+
+          vim.api.nvim_buf_call(args.buf, function()
+            lint.try_lint(names, opts)
+          end)
+        end)
+      )
     end,
   }
 )
